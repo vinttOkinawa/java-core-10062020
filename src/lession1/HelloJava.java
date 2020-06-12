cede336892d4bf5162dec5f978c4f42386cfbf8e
vinttOkinawa <nguyenvi.spk@gmail.com>
2020-06-13 00:50:26 +0900

    First commit

---
+++ src/lession1/HelloJava.java
@@ -0,0 +1,9 @@
+package lession1;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hihi Ngay dau tien di hoc");
+
+        System.out.println("Hello everyone");
+    }
+}
