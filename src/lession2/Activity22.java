1b4fdac7d42d4f4a276a5b681a278f649ef4ecaf
vinttOkinawa <nguyenvi.spk@gmail.com>
2020-06-17 00:25:00 +0900

    Lession2 - Activity22

--- src/lession2/Activity22.java
+++ src/lession2/Activity22.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 public class Activity22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.println();
+        int n = scanner.nextInt();
+        int n2 = n * 11;
+        int n3 = n * 111;
+        int tong = n + n2 + n3;
+        System.out.println(n + " + " + n2 + " + " + n3 + " = " + tong);
     }
 }
